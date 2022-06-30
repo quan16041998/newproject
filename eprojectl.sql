7a23692d4932c1c3b1cb273468307191c88d5ea3
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-06-30 23:22:25 +0700

    Create eprojectl.sql

---
+++ eprojectl.sql
@@ -0,0 +1,24 @@
+use 1808g;
+
+INSERT INTO product( product_code, fabric, price, size, urlimg, CollectionID, SID)
+VALUES 
+('GY_01', 'leather', 1500, 42, 'giay01.webp', 1,1),
+('GY_02', 'leather', 2000, 42, 'giay02.webp', 1,1),
+('GY_03', 'leather', 1600, 42, 'giay03.webp', 1,1),
+('GY_04', 'leather', 1500, 42, 'giay04.webp', 1,1),
+('GY_05', 'leather', 1300, 42, 'giay05.webp', 1,1),
+('GY_06', 'leather', 1500, 42, 'giay06.webp', 1,1),
+('GY_07', 'leather', 1100, 42, 'giay07.webp', 1,1),
+('GY_08', 'leather', 1500, 42, 'giay08.webp', 1,1),
+('GY_09', 'leather', 1200, 42, 'giay09.webp', 1,1),
+('GY_10', 'leather', 1300, 42, 'giay10.webp', 1,1),
+('GY_11', 'leather', 1500, 42, 'giay11.webp', 1,1),
+('GY_12', 'leather', 1400, 42, 'giay12.webp', 1,1),
+('GY_13', 'leather', 1500, 42, 'giay13.webp', 1,1),
+('GY_14', 'leather', 1400, 42, 'giay14.webp', 1,1),
+('GY_15', 'leather', 1500, 42, 'giay15.webp', 1,1),
+('GY_16', 'leather', 1800, 42, 'giay16.webp', 1,1),
+('GY_17', 'leather', 1800, 42, 'giay17.webp', 1,1),
+('GY_18', 'leather', 1700, 42, 'giay18.webp', 1,1),
+('GY_19', 'leather', 1500, 42, 'giay19.webp', 1,1),
+('GY_20', 'leather', 1900, 42, 'giay20.webp', 1,1);
