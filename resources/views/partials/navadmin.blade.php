50c4c64e4042d52e8d75a9856357000877af0b8c
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-06-30 08:50:24 +0700

    Update navadmin.blade.php

--- resources/views/partials/navadmin.blade.php
+++ resources/views/partials/navadmin.blade.php
@@ -1,41 +1,80 @@
-<nav class="navbar navbar-expand navbar-dark bg-info " style="margin-bottom:3rem ">
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark " style="margin-bottom:3rem ">
   <div class="container-fluid">
       <a class="navbar-brand" href="{{route('admin.homepage')}}">
-          <img src="{{asset('images/logo/download.jpg')}}" alt="Logo" style="width:40px;">
+          <img src="{{asset('images/download.jpg')}}" alt="Logo" style="width:40px;">
       </a>
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item me-3">
-          <div>
-
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="bookDropdown" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">
+            Admin Manager
+          </a>
+          <div class="dropdown-menu" aria-labelledby="bookDropdown">
+            <a class="dropdown-item" href="{{route('admin.adminindex')}}">View All</a>
           </div>
-      </li>
-      <li class="nav-item me-3">
-        <a class="nav-link" href="{{route('admin.homepage')}}">
-          <i class="bi bi-house-heart-fill"></i>
-          Home
-        </a>
-      </li>
-      <li class="nav-item">
-      <li class="nav-item">
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="formatDropdown" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">
+            Customer Manager
+          </a>
+          <div class="dropdown-menu" aria-labelledby="formatDropdown">
+            <a class="dropdown-item" href="{{route('admin.customerindex')}}">View All</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="publisherDropdown" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">
+            Stylist Manager
+          </a>
+          <div class="dropdown-menu" aria-labelledby="publisherDropdown">
+            <a class="dropdown-item" href="{{route('admin.stylistindex')}}">View All</a>
+            <a class="dropdown-item" href="{{route('admin.createstylist')}}">New Stylist</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="formatDropdown" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">
+            Product Manager
+          </a>
+          <div class="dropdown-menu" aria-labelledby="formatDropdown">
+            <a class="dropdown-item" href="{{route('admin.productindex')}}">View All</a>
+            <a class="dropdown-item" href="{{route('admin.createproduct')}}">New Product</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="formatDropdown" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">
+            Collection Manager
+          </a>
+          <div class="dropdown-menu" aria-labelledby="formatDropdown">
+            <a class="dropdown-item" href="{{route('admin.collectionindex')}}">View All</a>
+            <a class="dropdown-item" href="{{route('admin.createcollection')}}">New Collection</a>
+          </div>
+        </li>
 
-      </li>
-      <li class="nav-item mr-3">
-        <a class="nav-link" href="#">
-          <i class="bi bi-person"></i>
-          {{\Illuminate\Support\Facades\Session::has('username')?
-              \Illuminate\Support\Facades\Session::get('username') : ''}}
-        </a>
-      </li>
+        <ul class="navbar-nav mr-auto">
 
-      <li class="nav-item">
-        <a class="nav-link" href="{{route('admin.signout')}}">
-          <i class="bi bi-box-arrow-left"></i>
-          Logout
-        </a>
-      </li>
+          <li class="nav-item mr-3">
+            <a class="nav-link" href="#">
+              <i class="bi bi-person"></i>
+              {{\Illuminate\Support\Facades\Session::has('username')?
+                  \Illuminate\Support\Facades\Session::get('username') : ''}}
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('admin.signout')}}">
+              <i class="bi bi-box-arrow-left"></i>
+              Logout
+            </a>
+          </li>
 
 
-    </ul>
+        </ul>
+      </ul>
+    </div>
   </div>
 </nav>
 
