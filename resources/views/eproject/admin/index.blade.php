b7266b35902b84419de3d31809e794bc49cc24dc
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-03 11:13:14 +0700

    Update index.blade.php

--- resources/views/eproject/admin/index.blade.php
+++ resources/views/eproject/admin/index.blade.php
@@ -1,16 +1,14 @@
 @extends('masters.adminmaster')
 @section('main')
     @include('partials.allmessage')
-    <section class="page-section" id="index">
       <div class="card-body">
         <div class="card mb-4">
           <div class="card-header">
-            <i class="fas fa-table me-1"></i>
-            Admin Manager
+            <h2>Admin Index</h2>
           </div>
           <div class="card-body" >
             <div class="dataTable-container">
-              <table id="datatablesSimple" class="dataTable-table">
+              <table id="datatablesSimple" class="dataTable-table table-danger">
                 <thead>
                 <tr>
                   <th data-sortable="" style="width: 19.6495%;"><a>Username</a></th>
@@ -19,30 +17,30 @@
                   <th data-sortable="" style="width: 9.16091%;">&nbsp;</th>
                 </tr>
                 </thead>
-          <tbody>
-          @foreach($admin as $a)
-            <tr>
-              <td>{{$a->username}}</td>
-              <td>{{$a->name}}</td>
-              <td><a type="button" class="btn btn-info btn-sm"
-                     href="{{route('admin.showadmin', ['username' => $a->username])}}">
-                  Show</a>
-              </td>
-              <td><a type="button" class="btn btn-success btn-sm"
-                     href="{{route('admin.editadmin', ['username' => $a->username])}}">
-                  Edit
-                </a></td>
-            </tr>
-          @endforeach
-          </tbody>
-        </table>
-      </div>
+                <tbody>
+                @foreach($admin as $a)
+                  <tr>
+                    <td>{{$a->username}}</td>
+                    <td>{{$a->name}}</td>
+                    <td><a type="button" class="btn btn-info btn-sm"
+                           href="{{route('admin.showadmin', ['username' => $a->username])}}">
+                        <i class="bi bi-eye"></i>
+                      </a>
+                    </td>
+                    <td><a type="button" class="btn btn-success btn-sm"
+                           href="{{route('admin.editadmin', ['username' => $a->username])}}">
+                        <i class="bi bi-pencil-square"></i>
+                      </a>
+                    </td>
+                  </tr>
+                @endforeach
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
     </section>
-    </div>
-  </div>
 
 
 @endsection
