45e0056e0adec8828b96bed6da77095c1b8a971c
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-03 11:14:44 +0700

    Update index.blade.php

--- resources/views/eproject/stylist/index.blade.php
+++ resources/views/eproject/stylist/index.blade.php
@@ -6,18 +6,16 @@
     <div class="card mb-4">
       <div class="card-header">
         <i class="fas fa-table me-1"></i>
-        Stylist Manager
+        <h2>Stylist Index</h2>
       </div>
       <div class="card-body">
         <div class="dataTable-container">
-          <table id="datatablesSimple" class="dataTable-table">
+          <table id="datatablesSimple" class="dataTable-table table-info">
             <thead>
             <tr>
               <th data-sortable="" style="width: 19.6495%;"><a>Name</a></th>
               <th data-sortable="" style="width: 28.9432%;"><a>dob</a></th>
-              <th data-sortable="" style="width: 15.5337%;"><a type="button" class="btn btn-outline-info"
-                                                               href="{{route('admin.createstylist')}}">New Stylist</a>
-              </th>
+              <th data-sortable="" style="width: 15.5337%;">&nbsp;</th>
               <th data-sortable="" style="width: 9.16091%;">&nbsp;</th>
               <th data-sortable="" style="width: 9.16091%;">&nbsp;</th>
             </tr>
@@ -28,14 +26,20 @@
         <td>{{$a->name}}</td>
         <td>{{$a->dob}}</td>
         <td><a type="button" class="btn btn-info btn-sm"
-               href="{{route('admin.showstylist', ['id'=> $a->SID])}}"
-          >Show</a> </td>
+               href="{{route('admin.showstylist', ['id'=> $a->SID])}}">
+            <i class="bi bi-eye"></i>
+          </a>
+        </td>
         <td><a type="button" class="btn btn-success btn-sm"
-               href="{{route('admin.editstylist', ['id' => $a->SID])}}"
-          >Edit</a></td>
+               href="{{route('admin.editstylist', ['id' => $a->SID])}}">
+            <i class="bi bi-pencil-square"></i>
+          </a>
+        </td>
         <td><a type="button" class="btn btn-danger btn-sm"
-               href="{{route('admin.confirmstylist', ['id' => $a->SID])}}"
-          >Delete</a></td>
+               href="{{route('admin.confirmstylist', ['id' => $a->SID])}}">
+            <i class="bi bi-trash3"></i>
+          </a>
+        </td>
       </tr>
     @endforeach
     </tbody>
