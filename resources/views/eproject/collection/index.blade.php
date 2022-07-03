b71d7b97dccc6cf1498f3fa16ebb66abf733fc0a
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-03 11:13:42 +0700

    Update index.blade.php

--- resources/views/eproject/collection/index.blade.php
+++ resources/views/eproject/collection/index.blade.php
@@ -5,11 +5,11 @@
     <div class="card mb-4">
       <div class="card-header">
         <i class="fas fa-table me-1"></i>
-        Collection Manager
+        <h2>Collection Index</h2>
       </div>
       <div class="card-body">
         <div class="dataTable-container">
-          <table id="datatablesSimple" class="dataTable-table">
+          <table id="datatablesSimple" class="dataTable-table table-warning">
             <thead>
             <tr>
               <th data-sortable="" style="width: 28.9432%;"><a>Name</a></th>
@@ -23,14 +23,20 @@
               <tr>
                 <td>{{$c->name}}</td>
                 <td><a type="button" class="btn btn-info btn-sm"
-                       href="{{route('admin.showcollection', ['id'=> $c->CollectionID])}}"
-                  >Show</a> </td>
+                       href="{{route('admin.showcollection', ['id'=> $c->CollectionID])}}">
+                    <i class="bi bi-eye"></i>
+                  </a>
+                </td>
                 <td><a type="button" class="btn btn-success btn-sm"
-                       href="{{route('admin.editcollection', ['id'=> $c->CollectionID])}}"
-                  >Edit</a> </td>
+                       href="{{route('admin.editcollection', ['id'=> $c->CollectionID])}}">
+                    <i class="bi bi-pencil-square"></i>
+                  </a>
+                </td>
                 <td><a type="button" class="btn btn-danger btn-sm"
-                       href="{{route('admin.confirmcollection', ['id'=> $c->CollectionID])}}"
-                  >Delete</a> </td>
+                       href="{{route('admin.confirmcollection', ['id'=> $c->CollectionID])}}">
+                    <i class="bi bi-trash3"></i>
+                  </a>
+                </td>
               </tr>
             @endforeach
             </tbody>
