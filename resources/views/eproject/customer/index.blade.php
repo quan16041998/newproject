788597fdb04768984cb8399ddf2f4b9205ad0282
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-03 11:14:01 +0700

    Update index.blade.php

--- resources/views/eproject/customer/index.blade.php
+++ resources/views/eproject/customer/index.blade.php
@@ -6,11 +6,11 @@
     <div class="card mb-4">
       <div class="card-header">
         <i class="fas fa-table me-1"></i>
-        Customer Manager
+        <h2>Customer Index</h2>
       </div>
       <div class="card-body">
         <div class="dataTable-container">
-          <table id="datatablesSimple" class="dataTable-table">
+          <table id="datatablesSimple" class="dataTable-table table-success">
             <thead>
             <tr>
               <th data-sortable="" style="width: 28.9432%;"><a>Customer name</a></th>
@@ -24,10 +24,16 @@
               <tr>
                 <td>{{$a->name}}</td>
                 <td>{{$a->dob}}</td>
-                <td><a type="button" class="btn btn-primary btn-sm"
-                       href="{{route('admin.showcustomer',['id' => $a->CusID])}}">Show</a></td>
+                <td><a type="button" class="btn btn-info btn-sm"
+                       href="{{route('admin.showcustomer',['id' => $a->CusID])}}">
+                    <i class="bi bi-eye"></i>
+                  </a>
+                </td>
                 <td><a type="button" class="btn btn-success btn-sm"
-                       href="{{route('admin.editcustomer',['id' => $a->CusID])}}">Edit</a></td>
+                       href="{{route('admin.editcustomer',['id' => $a->CusID])}}">
+                    <i class="bi bi-pencil-square"></i>
+                  </a>
+                </td>
               </tr>
             @endforeach
             </tbody>
