634d727edf7c0512bce1faf150c67deb03cc7857
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-05 17:02:29 +0700

    Update create.blade.php

--- resources/views/eproject/collection/create.blade.php
+++ resources/views/eproject/collection/create.blade.php
@@ -1,42 +1,127 @@
-@extends('masters.adminmaster')
+@extends('masters.newmaster')
+
 @section('main')
-  <div class="container">
-    <h1 class="display-4">Create New Collection</h1>
-    @include('partials.ErrorsAll')
-    <form action="{{route('admin.storecollection')}}" method="post" enctype="multipart/form-data">
-      @csrf
-      <div class="card-body">
-        <div class="card mb-4">
-          <div class="card-header">
-            <i class="fas fa-table me-1"></i>
-          </div>
-          <div class="card-body">
-            <div class="dataTable-container">
-              <table id="datatablesSimple" class="dataTable-table">
-                <thead>
-                <tr><th><input type="hidden" name="id" value="{{old('id')?? $product->CID ?? null}}">
-                    <label for="name">Name</label>
-                  <th><input class="form-control" name="name" id="name" type="text" value="{{old('name')}}"></th>
-                </tr>
-                <tr>
-                  <th><label for="urlimg">URL Img</label></th>
-                  <th><input class="form-control" name="urlimg" id="urlimg" type="text" value="{{old('urlimg')}}"><input type="file" name="image"></th>
-                </tr>
-                <tr>
-                  <th><label for="introduce">Introduce</label></th>
-                  <th><input class="form-control" name="introduce" id="introduce" type="text" value="{{old('introduce')}}"></th>
-                </tr>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          @include('partials.ErrorsAll')
+          <form method="get" action="{{route('admin.storecollection', ['id' => old('id')?? $collection->CollectionID])}}"
+                autocomplete="off" class="form-horizontal">
+            @csrf
+            @method('put')
+
+            <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">{{ __('Create Profile') }}</h4>
+                <p class="card-category">{{ __('Collection information') }}</p>
+              </div>
+              <div class="card-body container">
+                @if (session('status'))
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <i class="material-icons">close</i>
+                        </button>
+                        <span>{{ session('status') }}</span>
+                      </div>
+                    </div>
+                  </div>
+                @endif
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Name') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input class="form-control" name="name" id="name" type="text placeholder="{{ __('Name') }}"
+                      value="{{old('name')?? $collection->name}}" />
+                    </div>
+                  </div>
+                </div>
 
 
-                </thead>
-              </table>
+
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('introduce') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input class="form-control" name="contact" id="contact" type="text" placeholder="{{ __('introduce') }}"
+                             value="{{old('introduce')?? $collection->introduce}}"  />
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer ml-auto mr-auto">
+                  <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+                </div>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
-        <button type="submit" class="btn btn-dark">Submit</button>
-        <a type="button" href="{{route('admin.collectionindex')}}" class="btn btn-info">&lt;&lt;&nbsp;Back</a>
-    </form>
-
+    </div>
   </div>
+  {{--      <div class="row">--}}
+  {{--        <div class="col-md-12">--}}
+  {{--          <form method="post" action="{{ route('profile.password') }}" class="form-horizontal">--}}
+  {{--            @csrf--}}
+  {{--            @method('put')--}}
+
+  {{--            <div class="card ">--}}
+  {{--              <div class="card-header card-header-primary">--}}
+  {{--                <h4 class="card-title">{{ __('Change password') }}</h4>--}}
+  {{--                <p class="card-category">{{ __('Password') }}</p>--}}
+  {{--              </div>--}}
+  {{--              <div class="card-body ">--}}
+  {{--                @if (session('status_password'))--}}
+  {{--                  <div class="row">--}}
+  {{--                    <div class="col-sm-12">--}}
+  {{--                      <div class="alert alert-success">--}}
+  {{--                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">--}}
+  {{--                          <i class="material-icons">close</i>--}}
+  {{--                        </button>--}}
+  {{--                        <span>{{ session('status_password') }}</span>--}}
+  {{--                      </div>--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                @endif--}}
+  {{--                <div class="row">--}}
+  {{--                  <label class="col-sm-2 col-form-label" for="input-current-password">{{ __('Current Password') }}</label>--}}
+  {{--                  <div class="col-sm-7">--}}
+  {{--                    <div class="form-group{{ $errors->has('old_password') ? ' has-danger' : '' }}">--}}
+  {{--                      <input class="form-control{{ $errors->has('old_password') ? ' is-invalid' : '' }}" input type="password" name="old_password" id="input-current-password" placeholder="{{ __('Current Password') }}" value="" required />--}}
+  {{--                      @if ($errors->has('old_password'))--}}
+  {{--                        <span id="name-error" class="error text-danger" for="input-name">{{ $errors->first('old_password') }}</span>--}}
+  {{--                      @endif--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                </div>--}}
+  {{--                <div class="row">--}}
+  {{--                  <label class="col-sm-2 col-form-label" for="input-password">{{ __('New Password') }}</label>--}}
+  {{--                  <div class="col-sm-7">--}}
+  {{--                    <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">--}}
+  {{--                      <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="input-password" type="password" placeholder="{{ __('New Password') }}" value="" required />--}}
+  {{--                      @if ($errors->has('password'))--}}
+  {{--                        <span id="password-error" class="error text-danger" for="input-password">{{ $errors->first('password') }}</span>--}}
+  {{--                      @endif--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                </div>--}}
+  {{--                <div class="row">--}}
+  {{--                  <label class="col-sm-2 col-form-label" for="input-password-confirmation">{{ __('Confirm New Password') }}</label>--}}
+  {{--                  <div class="col-sm-7">--}}
+  {{--                    <div class="form-group">--}}
+  {{--                      <input class="form-control" name="password_confirmation" id="input-password-confirmation" type="password" placeholder="{{ __('Confirm New Password') }}" value="" required />--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                </div>--}}
+  {{--              </div>--}}
+  {{--              <div class="card-footer ml-auto mr-auto">--}}
+  {{--                <button type="submit" class="btn btn-primary">{{ __('Change password') }}</button>--}}
+  {{--              </div>--}}
+  {{--            </div>--}}
+  {{--          </form>--}}
+  {{--        </div>--}}
+  {{--      </div>--}}
+  {{--    </div>--}}
+  {{--  </div>--}}
 @endsection
