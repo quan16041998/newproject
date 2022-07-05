f434b035c77c107a0a054d476c4963cd616a7873
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-06 00:24:31 +0700

    Update edit.blade.php

--- resources/views/eproject/admin/edit.blade.php
+++ resources/views/eproject/admin/edit.blade.php
@@ -2,13 +2,16 @@
 
 @section('main')
   <div class="content">
-      @include('partials.ErrorsAll')
-      @include('partials.sessionmessage')
+    @include('partials.ErrorsAll')
+    @include('partials.sessionmessage')
+    @include('partials.allmessage')
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
-          <form method="post" action="{{route('admin.updateadmin', ['username' =>$admin->username])}}" autocomplete="off" class="form-horizontal">
+          <form method="post" action="{{route('admin.updateadmin', ['username' => old('username')?? $admin->username])}}"
+                autocomplete="off" class="form-horizontal">
             @csrf
+
             <div class="card ">
               <div class="card-header card-header-primary">
                 <h4 class="card-title">{{ __('Edit Profile') }}</h4>
@@ -27,12 +30,15 @@
                     </div>
                   </div>
                 @endif
+
+                  <input type="hidden" name="username" value="{{$admin->username}}">
+
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('User Name') }}</label>
                   <div class="col-sm-7">
                     <div class="form-group">
-                      <input class="form-control"  type="text" placeholder="{{ __('UserName') }}" value="{{ old('username')?? $admin->username }}" disabled />
-                        <input type="hidden" name="username" id="username" value="{{ old('username')?? $admin->username }}">
+                      <input class="form-control" name="username" id="username" type="text"
+                             value="{{ old('username')?? $admin->username }}" />
                     </div>
                   </div>
                 </div>
@@ -40,7 +46,8 @@
                   <label class="col-sm-2 col-form-label">{{ __('Name') }}</label>
                   <div class="col-sm-7">
                     <div class="form-group">
-                      <input class="form-control" name="name" id="name" type="text placeholder="{{ __('Name') }}" value="{{old('name')?? $admin->name}}" />
+                      <input class="form-control" name="name" id="name" type="text"
+                             value="{{old('name')?? $admin->name}}" />
                     </div>
                   </div>
                 </div>
@@ -48,7 +55,8 @@
                         <label class="col-sm-2 col-form-label">{{ __('Data Of Birth') }}</label>
                         <div class="col-sm-7">
                             <div class="form-group">
-                                <input class="form-control" name="dob" id="dob" type="date" placeholder="{{ __('DOB') }}" value="{{old('dob')?? $admin->dob}}"  />
+                                <input class="form-control" name="dob" id="dob" type="date"
+                                       value="{{old('dob')?? $admin->dob}}"  />
                             </div>
                         </div>
                     </div>
@@ -57,7 +65,8 @@
                     <label class="col-sm-2 col-form-label">{{ __('Contact') }}</label>
                     <div class="col-sm-7">
                         <div class="form-group">
-                            <input class="form-control" name="contact" id="contact" type="text" placeholder="{{ __('Contact') }}" value="{{old('contact')?? $admin->contact}}"  />
+                            <input class="form-control" name="contact" id="contact" type="text"
+                                   value="{{old('contact')?? $admin->contact}}"  />
                         </div>
                     </div>
                 </div>
@@ -65,7 +74,8 @@
                     <label class="col-sm-2 col-form-label">{{ __('Email') }}</label>
                     <div class="col-sm-7">
                         <div class="form-group">
-                            <input class="form-control" name="email" id="input-email" type="email" placeholder="{{ __('Email') }}" value="{{old('email')?? $admin->email}}" />
+                            <input class="form-control" name="email" id="input-email" type="email"
+                                   value="{{old('email')?? $admin->email}}" />
                         </div>
                     </div>
                 </div>
@@ -95,5 +105,68 @@
       </div>
     </div>
   </div>
+{{--      <div class="row">--}}
+{{--        <div class="col-md-12">--}}
+{{--          <form method="post" action="{{ route('profile.password') }}" class="form-horizontal">--}}
+{{--            @csrf--}}
+{{--            @method('put')--}}
 
+{{--            <div class="card ">--}}
+{{--              <div class="card-header card-header-primary">--}}
+{{--                <h4 class="card-title">{{ __('Change password') }}</h4>--}}
+{{--                <p class="card-category">{{ __('Password') }}</p>--}}
+{{--              </div>--}}
+{{--              <div class="card-body ">--}}
+{{--                @if (session('status_password'))--}}
+{{--                  <div class="row">--}}
+{{--                    <div class="col-sm-12">--}}
+{{--                      <div class="alert alert-success">--}}
+{{--                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">--}}
+{{--                          <i class="material-icons">close</i>--}}
+{{--                        </button>--}}
+{{--                        <span>{{ session('status_password') }}</span>--}}
+{{--                      </div>--}}
+{{--                    </div>--}}
+{{--                  </div>--}}
+{{--                @endif--}}
+{{--                <div class="row">--}}
+{{--                  <label class="col-sm-2 col-form-label" for="input-current-password">{{ __('Current Password') }}</label>--}}
+{{--                  <div class="col-sm-7">--}}
+{{--                    <div class="form-group{{ $errors->has('old_password') ? ' has-danger' : '' }}">--}}
+{{--                      <input class="form-control{{ $errors->has('old_password') ? ' is-invalid' : '' }}" input type="password" name="old_password" id="input-current-password" placeholder="{{ __('Current Password') }}" value="" required />--}}
+{{--                      @if ($errors->has('old_password'))--}}
+{{--                        <span id="name-error" class="error text-danger" for="input-name">{{ $errors->first('old_password') }}</span>--}}
+{{--                      @endif--}}
+{{--                    </div>--}}
+{{--                  </div>--}}
+{{--                </div>--}}
+{{--                <div class="row">--}}
+{{--                  <label class="col-sm-2 col-form-label" for="input-password">{{ __('New Password') }}</label>--}}
+{{--                  <div class="col-sm-7">--}}
+{{--                    <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">--}}
+{{--                      <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="input-password" type="password" placeholder="{{ __('New Password') }}" value="" required />--}}
+{{--                      @if ($errors->has('password'))--}}
+{{--                        <span id="password-error" class="error text-danger" for="input-password">{{ $errors->first('password') }}</span>--}}
+{{--                      @endif--}}
+{{--                    </div>--}}
+{{--                  </div>--}}
+{{--                </div>--}}
+{{--                <div class="row">--}}
+{{--                  <label class="col-sm-2 col-form-label" for="input-password-confirmation">{{ __('Confirm New Password') }}</label>--}}
+{{--                  <div class="col-sm-7">--}}
+{{--                    <div class="form-group">--}}
+{{--                      <input class="form-control" name="password_confirmation" id="input-password-confirmation" type="password" placeholder="{{ __('Confirm New Password') }}" value="" required />--}}
+{{--                    </div>--}}
+{{--                  </div>--}}
+{{--                </div>--}}
+{{--              </div>--}}
+{{--              <div class="card-footer ml-auto mr-auto">--}}
+{{--                <button type="submit" class="btn btn-primary">{{ __('Change password') }}</button>--}}
+{{--              </div>--}}
+{{--            </div>--}}
+{{--          </form>--}}
+{{--        </div>--}}
+{{--      </div>--}}
+{{--    </div>--}}
+{{--  </div>--}}
 @endsection
