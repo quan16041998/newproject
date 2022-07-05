85eb9637b260fb27f08b16af7bcc3ad8e62b30db
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-05 17:01:58 +0700

    Update delete.blade.php

--- resources/views/eproject/collection/delete.blade.php
+++ resources/views/eproject/collection/delete.blade.php
@@ -1,29 +1,51 @@
-@extends('masters.adminmaster')
+@extends('masters.newmaster')
 @section('main')
-  <div class="container">
-    <form action="{{route('admin.deletecollection', ['id' => $collection->CollectionID])}}" method="post">
-      @csrf
-      <dl class="row">
-        <div class="col-sm-6">
-          <dt class="col-sm-3">ID</dt>
-          <dd class="col-sm-9">{{ $collection->CollectionID }}</dd>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <form method="get" action="{{route('admin.deletecollection', ['id' => old('id')?? $collection->CollectionID ])}}" autocomplete="off" class="form-horizontal">
+            @csrf
+            @method('put')
 
-          <dt class="col-sm-3">Name</dt>
-          <dd class="col-sm-9">{{ $collection->name }}</dd>
+            <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">{{ __('Collection Delete') }}</h4>
+                <p class="card-category">{{ __('Collection information') }}</p>
+              </div>
+              <div class="card-body ">
+                @if (session('status'))
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <i class="material-icons">close</i>
+                        </button>
+                        <span>{{ session('status') }}</span>
+                      </div>
+                    </div>
+                  </div>
+                @endif
+                <dl class="row">
+                  <dt class="col-sm-3">Name</dt>
+                  <dd class="col-sm-9">{{ $collection->name }}</dd>
 
-          <dt class="col-sm-3">Introduce</dt>
-          <dd class="col-sm-9">{{$collection->introduce }}</dd>
+                  <dt class="col-sm-3">Stylist</dt>
+                  <dd class="col-sm-9">{{ $collection->stylist }}</dd>
 
+                  <dt class="col-sm-3">Introduct</dt>
+                  <dd class="col-sm-9">{{ $collection->introduce  }}</dd>
+
+                </dl>
+                  <div class="card-footer ml-auto mr-auto">
+                    <button type="submit" class="btn btn-primary">{{ __('Delete') }}</button>
+                  </div>
+              </div>
+              </div>
+          </form>
         </div>
-        <div class="col-sm-6">
-          <img src="{{asset($collection->urlimg)}}" width="350" height="400">
         </div>
-      </dl>
-
-      <input type="hidden" name="id" value="{{$collection->CollectionID}}">
-      <input type="submit" class="btn btn-danger" value="Delete">
-      <a type="button" href="{{route('admin.collectionindex')}}" class="btn btn-info">&lt;&lt;&nbsp;Back</a>
-    </form>
-
+      </div>
+    </div>
   </div>
 @endsection
