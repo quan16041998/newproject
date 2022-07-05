9bfcc1c341a528cf716a82479b9263e6725b470b
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-05 17:07:11 +0700

    Update update.blade.php

--- resources/views/eproject/stylist/update.blade.php
+++ resources/views/eproject/stylist/update.blade.php
@@ -1,17 +1,168 @@
-@extends('masters.adminmaster')
+@extends('masters.newmaster')
 
 @section('main')
-  <div class="container">
-    <h1 class="display-4">Update An Existing Stylist</h1>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <form method="get" action="{{route('admin.updatestylist', ['id' => old('id')?? $stylist->SID])}}"
+                autocomplete="off" class="form-horizontal">
+            @csrf
+            @method('put')
 
-    @include('partials.ErrorsAll')
+            <div class="card ">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title">{{ __('Edit Profile') }}</h4>
+                <p class="card-category">{{ __('Stylist information') }}</p>
+              </div>
+              <div class="card-body container">
+                @if (session('status'))
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <i class="material-icons">close</i>
+                        </button>
+                        <span>{{ session('status') }}</span>
+                      </div>
+                    </div>
+                  </div>
+                @endif
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Stylist name') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input class="form-control" name="name" id="name" type="text placeholder="{{ __('Name') }}"
+                      value="{{old('name')?? $stylist->name}}" />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Data Of Birth') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input class="form-control" name="dob" id="dob" type="date" placeholder="{{ __('DOB') }}"
+                             value="{{old('dob')?? $stylist->dob}}"  />
+                    </div>
+                  </div>
+                </div>
 
-    <form action="{{route('admin.updatestylist', ['id' => old('id')?? $stylist->SID])}}" method="post" enctype="multipart/form-data">
-      @csrf
-      @include('eproject.stylist.StylistField')
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Contact') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input class="form-control" name="contact" id="contact" type="text" placeholder="{{ __('Contact') }}"
+                             value="{{old('contact')?? $stylist->contact}}"  />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('Email') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input class="form-control" name="email" id="input-email" type="email" placeholder="{{ __('Email') }}"
+                             value="{{old('email')?? $stylist->email}}" />
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">{{ __('History') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input class="form-control" name="text" id="input-history" type="history" placeholder="{{ __('history') }}"
+                             value="{{old('history')?? $stylist->history}}" />
+                    </div>
+                  </div>
+                </div>
 
-      <button type="submit" class="btn btn-dark">Submit</button>
-      <a href="{{route('admin.stylistindex')}}" class="btn btn-info">&lt;&lt;&nbsp;Back</a>
-    </form>
+                  @php
+                    $img = 'images/stylist/';
+                   $sid = old('stylist') ??  $product->SID ??  null;
+                   $cid = old('collection') ?? $product->CollectionID  ?? null;
+                  @endphp
+
+                  <div class="row">
+                    <label class="col-sm-2 col-form-label">{{ __('Urlimg') }}</label>
+                    <div class="col-sm-7">
+                      <div class="form-group">
+                        <input class="form-control" name="urlimg" id="input-history" type="urlimg" placeholder="{{ __('urlimg') }}"
+                               value="{{old('urlimg')?? $stylist->urlimg}}" /><input type="file" name="image"><br>
+                        <img src="{{asset($img.$stylist->urlimg)}}" width="350" height="400">
+                      </div>
+                    </div>
+                  </div>
+                <div class="card-footer ml-auto mr-auto">
+                  <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
+  {{--      <div class="row">--}}
+  {{--        <div class="col-md-12">--}}
+  {{--          <form method="post" action="{{ route('profile.password') }}" class="form-horizontal">--}}
+  {{--            @csrf--}}
+  {{--            @method('put')--}}
+
+  {{--            <div class="card ">--}}
+  {{--              <div class="card-header card-header-primary">--}}
+  {{--                <h4 class="card-title">{{ __('Change password') }}</h4>--}}
+  {{--                <p class="card-category">{{ __('Password') }}</p>--}}
+  {{--              </div>--}}
+  {{--              <div class="card-body ">--}}
+  {{--                @if (session('status_password'))--}}
+  {{--                  <div class="row">--}}
+  {{--                    <div class="col-sm-12">--}}
+  {{--                      <div class="alert alert-success">--}}
+  {{--                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">--}}
+  {{--                          <i class="material-icons">close</i>--}}
+  {{--                        </button>--}}
+  {{--                        <span>{{ session('status_password') }}</span>--}}
+  {{--                      </div>--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                @endif--}}
+  {{--                <div class="row">--}}
+  {{--                  <label class="col-sm-2 col-form-label" for="input-current-password">{{ __('Current Password') }}</label>--}}
+  {{--                  <div class="col-sm-7">--}}
+  {{--                    <div class="form-group{{ $errors->has('old_password') ? ' has-danger' : '' }}">--}}
+  {{--                      <input class="form-control{{ $errors->has('old_password') ? ' is-invalid' : '' }}" input type="password" name="old_password" id="input-current-password" placeholder="{{ __('Current Password') }}" value="" required />--}}
+  {{--                      @if ($errors->has('old_password'))--}}
+  {{--                        <span id="name-error" class="error text-danger" for="input-name">{{ $errors->first('old_password') }}</span>--}}
+  {{--                      @endif--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                </div>--}}
+  {{--                <div class="row">--}}
+  {{--                  <label class="col-sm-2 col-form-label" for="input-password">{{ __('New Password') }}</label>--}}
+  {{--                  <div class="col-sm-7">--}}
+  {{--                    <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">--}}
+  {{--                      <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="input-password" type="password" placeholder="{{ __('New Password') }}" value="" required />--}}
+  {{--                      @if ($errors->has('password'))--}}
+  {{--                        <span id="password-error" class="error text-danger" for="input-password">{{ $errors->first('password') }}</span>--}}
+  {{--                      @endif--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                </div>--}}
+  {{--                <div class="row">--}}
+  {{--                  <label class="col-sm-2 col-form-label" for="input-password-confirmation">{{ __('Confirm New Password') }}</label>--}}
+  {{--                  <div class="col-sm-7">--}}
+  {{--                    <div class="form-group">--}}
+  {{--                      <input class="form-control" name="password_confirmation" id="input-password-confirmation" type="password" placeholder="{{ __('Confirm New Password') }}" value="" required />--}}
+  {{--                    </div>--}}
+  {{--                  </div>--}}
+  {{--                </div>--}}
+  {{--              </div>--}}
+  {{--              <div class="card-footer ml-auto mr-auto">--}}
+  {{--                <button type="submit" class="btn btn-primary">{{ __('Change password') }}</button>--}}
+  {{--              </div>--}}
+  {{--            </div>--}}
+  {{--          </form>--}}
+  {{--        </div>--}}
+  {{--      </div>--}}
+  {{--    </div>--}}
+  {{--  </div>--}}
 @endsection
