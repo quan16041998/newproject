6bb5508b3a66473224009efda51bb6195496df80
tuanbui03 <101034908+tuanbui03@users.noreply.github.com>
2022-07-05 20:27:47 +0700

    Create login.blade.php

---
+++ resources/views/eproject/viewC1/login.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>RegistrationForm_v1 by Colorlib</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- MATERIAL DESIGN ICONIC FONT -->
+    <link rel="stylesheet" href="{{asset('fonts/material-design-iconic-font/css/material-design-iconic-font.min.css')}}">
+
+    <!-- STYLE CSS -->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+
+<body>
+
+<div class="wrapper" style="background-image: url({{asset('images/bg-registration-form-1.jpg')}});">
+    <div class="inner">
+        <div class="image-holder">
+            <img src="{{asset('images/registration-form-1.jpg')}}" alt="">
+        </div>
+        <form action="{{route('viewC1.login')}}" method="get">
+            <h3>Welcome</h3>
+            <div class="form-wrapper">
+                <input type="text" placeholder="Email" class="form-control" value="{{old('email')?? null}}">
+                <i class="zmdi zmdi-account"></i>
+            </div>
+            <div class="form-wrapper">
+                <input type="password" placeholder="Password" class="form-control">
+                <i class="zmdi zmdi-lock"></i>
+            </div>
+
+            <button>Login
+                <i class="zmdi zmdi-arrow-right"></i>
+            </button>
+            <button href="{{route('viewC1.signup')}}">Register
+                <i class="zmdi zmdi-arrow-right"></i>
+            </button>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
