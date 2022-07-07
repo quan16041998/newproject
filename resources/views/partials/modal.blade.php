46d500bfb08d59a5b43388bac143f07ebd108fbb
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-07 16:27:08 +0700

    Update modal.blade.php

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -10,16 +10,14 @@
                                         </div>
                                         <div class="col-lg-7 p-lg-0"><a class="lightbox product-view d-block h-50 bg-cover bg-center w-40" style="background: url({{asset('images/ViewC1/00001-burberry-fall-2022-menswear-credit-brand.webp')}})" href="img/product-5.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="lightbox d-none" href="img/product-5-alt-1.jpg" data-gallery="gallery1" data-glightbox="HaHa"></a><a class="glightbox d-none" href="img/product-5-alt-2.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a></div>
                                     </div>
-
                                     <h2 class="h4 text-center" id="show1">MD_QA01</h2>
-                                    <p class="text-muted">fabric: velvet</p>
-                                    <p class="text-muted">price: 1500$</p>
-                                    <p class="text-muted">Size: L</p>
-                                    <p class="text-muted">Collection: Winter 2022</p>
-                                    <p class="text-muted">Stylist: Dinh Tien Quan</p>
-                                    <p class="text-sm mb-4"></p>
+                                    <p class="text-sm">Fabric: velvet</p>
+                                    <p class="text-sm">Price: 1500$</p>
+                                    <p class="text-sm">Size: L</p>
+                                    <p class="text-sm">Collection: Winter 2022</p>
+                                    <p class="text-sm">Stylist: Dinh Tien Quan</p>
+                                    <p class="text-sm"></p>
                                     <a class="btn btn-link text-dark text-decoration-none p-0" href="#!"><i class="far fa-heart me-2"></i>QTK</a>
-
                                  </div>
                                 <div class="col-sm-2 my-lg-5">
                                     <h1><i class="bi bi-caret-right"></i><i class="bi bi-caret-left"></i></h1>
@@ -29,23 +27,25 @@
                                         <div class="col-lg-2">
                                         </div>
                                         <div class="col-lg-7 p-lg-0"><a class="lightbox product-view d-block h-100 bg-cover bg-center" style="background: url({{asset('images/product/00009-DOLCE-GABBANA-MENSWEAR-SPRING-21.webp')}})" href="img/product-5.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="lightbox d-none" href="img/product-5-alt-1.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a><a class="glightbox d-none" href="img/product-5-alt-2.jpg" data-gallery="gallery1" data-glightbox="Red digital smartwatch"></a></div>
-
                                     </div>
                                     <h2 class="h4" id="show1">MH_QA09</h2>
-                                    <p class="text-muted">fabric: cotton</p>
-                                    <p class="text-muted">price: 2500$</p>
-                                    <p class="text-muted">Size: L</p>
-                                    <p class="text-muted">Collection: Summer 2022</p>
-                                    <p class="text-muted">Stylist: Do Khac Duong</p>
+                                    <p class="text-sm">Fabric: cotton</p>
+                                    <p class="text-sm">Price: 2500$</p>
+                                    <p class="text-sm">Size: L</p>
+                                    <p class="text-sm">Collection: Summer 2022</p>
+                                    <p class="text-sm">Stylist: Do Khac Duong</p>
                                     <p class="text-sm mb-4"></p>
 
                                     <a class="btn btn-link text-dark text-decoration-none p-0" href="#!"><i class="far fa-heart me-2"></i>QTK</a>
                                 </div>
+
+
                 </div>>
                 </div>
             </div>
     </div>
 </div>
+
 
 
 
