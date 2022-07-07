2612c8a1be7bff529c66146bc3eaa30ffb4ae708
tuanbui03 <101034908+tuanbui03@users.noreply.github.com>
2022-07-07 09:14:57 +0700

    Update footerViewC1.blade.php

--- resources/views/partials/footerViewC1.blade.php
+++ resources/views/partials/footerViewC1.blade.php
@@ -28,6 +28,13 @@
                     <li><a class="text-white reset-anchor" style=" text-decoration: none" href="https://www.pinterest.com/">Pinterest</a></li>
                 </ul>
             </div>
+            <div class="col-md-3">
+                <h6 class="text-uppercase mb-3">Maps</h6>
+                <ul class="list-unstyled mb-0">
+                    <li><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.729588370534!2d105.84690551420148!3d21.00347399401861!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb7d4993d5b02357e!2sAptech%20Computer%20Education!5e0!3m2!1svi!2s!4v1657158913345!5m2!1svi!2s"
+                                width="200" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></li>
+                </ul>
+            </div>
         </div>
         <div class="border-top pt-4" style="border-color: #1d1d1d !important">
             <div class="row">
