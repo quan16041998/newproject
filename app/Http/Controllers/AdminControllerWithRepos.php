6250996bbed4f41329e099e6a1d7ef0315e60a31
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-08 22:32:31 +0700

    Update AdminControllerWithRepos.php

--- app/Http/Controllers/AdminControllerWithRepos.php
+++ app/Http/Controllers/AdminControllerWithRepos.php
@@ -411,15 +411,16 @@ class   AdminControllerWithRepos extends Controller
                 'dob' => ['required'],
                 'contact' => ['required'],
                 'email' => ['required'],
-                'history' => ['required']
+                'history' => ['required'],
+                'image' => ['required']
             ],
             [
                 'name.required' => 'Stylist name can not be empty',
-                'dob.required' => 'dob can not be empty',
-                'contact.required' => 'contact can not be empty',
-                'email.required' => 'email can not be empty',
-                'history.required' => 'history can not be empty',
-                'urlimg.required' => 'urlimg can not be empty',
+                'dob.required' => 'DOB can not be empty',
+                'contact.required' => 'Contact can not be empty',
+                'email.required' => 'Email can not be empty',
+                'history.required' => 'History can not be empty',
+                'image.required' => 'Chose File for image can not be empty',
 
             ]
         );
@@ -592,18 +593,18 @@ class   AdminControllerWithRepos extends Controller
                 'fabric' => ['required'],
                 'price' => ['required'],
                 'size' => ['required'],
-
-                'CollectionID' => ['required'],
-                'SID' => ['required']
+                'image' => ['required'},
+                'CollectionID' => ['gt:0'],
+                'SID' => ['gt:0']
             ],
             [
                 'product_code.required' => 'Product code can not be empty',
                 'fabric.required' => 'Fabric can not be empty',
                 'price.required' => 'Price can not be empty',
                 'size.required' => 'Size can not be empty',
-                'urlimg.required' => 'Url for image can not be empty',
-                'CollectionID.required' => 'Conllection ID can not be empty',
-                'SID.required' => 'SID can not be empty',
+                'image.required' => 'Chose file for image can not be empty',
+                'CollectionID.gt' => 'Conllection ID can not be empty',
+                'SID.gt' => 'SID can not be empty',
             ]
         );
     }
