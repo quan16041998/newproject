058939a0aaf28ce18f07a776bac5f98a24ce0d99
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-07-08 21:33:02 +0700

    Update create.blade.php

--- resources/views/eproject/product/create.blade.php
+++ resources/views/eproject/product/create.blade.php
@@ -80,6 +80,8 @@
                   <label class="col-sm-2 col-form-label">{{ __('Urlimg') }}</label>
                   <div class="col-sm-7">
                     <div class="form-group">
+                      <input type="hidden" class="form-control" id="urlimg" name="urlimg"
+                             value="{{old('urlimg')?? $product->urlimg?? null}}">
                       <img src="{{asset($img.$product->urlimg)}}" width="350" height="400">
                     </div>
                   </div>
